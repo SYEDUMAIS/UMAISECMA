d555fc52268bd7a99e0028b58b8f0a8f569c3e22
SYEDUMAIS <syed.umaismgm@gmail.com>
2021-07-12 13:31:41 +0530

    js commit

---
+++ BROWSER/diff.js
@@ -0,0 +1,27 @@
+function mynamevar(){
+    if(true){
+        var name="syed";
+    }
+
+    console.log(name);
+}
+mynamevar();
+
+// function mynamelet(){
+//     if(true){
+//         let name="syed";
+//     }
+//     console.log(name);
+// }
+// mynamelet();
+
+function mynamelet(){
+    if(true){
+        let name="syed";
+        console.log(name);
+    }
+    
+}
+mynamelet();
+
+
---
+++ BROWSER/firstcode.js
@@ -0,0 +1,1 @@
+console.log("I'm using Ecma scripting on VS Code");
