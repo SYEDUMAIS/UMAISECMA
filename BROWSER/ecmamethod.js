93eb0df986b01209e9c4e42fffa09b76864a84bc
SYEDUMAIS <syed.umaismgm@gmail.com>
2021-07-13 12:50:24 +0530

    ecmacommit

---
+++ BROWSER/ecmamethod.js
@@ -0,0 +1,7 @@
+function calculate1(num1, num2) {
+  return num1 + num2;
+}
+console.log(`${calculate1(20, 12)}`);
+
+const calc = (num1, num2) => num1 + num2;
+console.log(`${calculate1(25, 16)}`);
