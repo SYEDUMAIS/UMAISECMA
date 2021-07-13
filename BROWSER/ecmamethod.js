53dc2b0ffd25e45814861ee80196d2b9bf4f5034
SYEDUMAIS <syed.umaismgm@gmail.com>
2021-07-13 13:32:04 +0530

    compair

--- BROWSER/ecmamethod.js
+++ BROWSER/ecmamethod.js
@@ -1,7 +1,11 @@
-function calculate1(num1, num2) {
-  return num1 + num2;
-}
-console.log(`${calculate1(20, 12)}`);
+// function calculate1(num1, num2) {
+//   return num1 + num2;
+// }
+// console.log(`${calculate1(20, 12)}`);
 
-const calc = (num1, num2) => num1 + num2;
-console.log(`${calculate1(25, 16)}`);
+// const calc = (num1, num2) => num1 + num2;
+// console.log(`${calculate1(25, 16)}`);
+
+const biggernum=(num1,num2)=> (num1>num2)?num1:num2;
+
+console.log(`${biggernum(8,12)}`);
